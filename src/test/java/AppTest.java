38e06c9439e43711e265d185ab03c8fe62d4641d
Ariel Spear & Ashley Sullins <student@epicodus.com>
2015-08-13 11:13:34 -0700

    tests dimes

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -9,4 +9,10 @@ public class AppTest {
     Integer quarters = 1;
     assertEquals(quarters, testApp.makeChange(25));
   }
+
+  @Test
+  public void makeChange_ifMoneyIsGreaterThan10CentsAddDime_2() {
+    Integer dimes = 2;
+    assertEquals(dimes, testApp.makeChange(20));
+  }
 }
