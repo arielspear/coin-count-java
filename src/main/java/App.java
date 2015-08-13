617b1e3edc999d9d19fa844dc4bda6c540c63ccf
Ariel Spear & Ashley Sullins <student@epicodus.com>
2015-08-13 12:01:55 -0700

    Counts dimes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,25 +1,33 @@
-
+import java.io.Console;
 import static spark.Spark.*;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 public class App {
 
+  Console myConsole = System.console();
   public static void main(String[] args) {
   }
 
-  public Integer makeChange(Integer valueInCents){
+  public int[] makeChange(int valueInCents){
 
-    Integer quarters = 0;
-    Integer dimes = 0;
-    Integer nickles = 0;
-    Integer pennies = 0;
-
+    int quarters = 0;
+    int dimes = 0;
+    int nickles = 0;
+    int pennies = 0;
 
     while (valueInCents >= 25) {
-     valueInCents = valueInCents - 25;
-     quarters = quarters + 1;
+      valueInCents = valueInCents - 25;
+      quarters = quarters + 1;
     }
-    return quarters;
+
+    while (valueInCents >= 10 ) {
+      valueInCents = valueInCents - 10;
+      dimes = dimes + 1;
+    }
+
+    int coins[] = {quarters, dimes, nickles, pennies};
+
+    return coins;
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,18 +1,23 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.io.Console;
+import java.util.Arrays;
 
 public class AppTest {
+
+  Console myConsole = System.console();
+
   App testApp = new App();
 
   @Test
   public void makeChange_ifMoneyIsGreaterThan25CentsAddQuarter_1() {
-    Integer quarters = 1;
-    assertEquals(quarters, testApp.makeChange(25));
+    int[] result = {1,0,0,0};
+    assertTrue(Arrays.equals(result,testApp.makeChange(25)));
   }
 
   @Test
   public void makeChange_ifMoneyIsGreaterThan10CentsAddDime_2() {
-    Integer dimes = 2;
-    assertEquals(dimes, testApp.makeChange(20));
+    int[] result = {0,2,0,0};
+    assertTrue(Arrays.equals(result,testApp.makeChange(20)));
   }
 }
